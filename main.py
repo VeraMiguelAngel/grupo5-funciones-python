1786cfd591007f65df333e0196a8236f3562fdcb
VeraMiguelAngel <miguel_vera_a@yahoo.com.ar>
2025-10-30 14:38:07 -0300

    primer paso de la actividad

---
+++ main.py
@@ -0,0 +1,4 @@
+# Cada integrante deberá agregar su función en un archivo independiente
+from funciones import *
+if __name__ == "__main__":
+ print("Ejecución de pruebas de las funciones del grupo")
