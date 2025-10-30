ecf099d28d0da75087577c2348802df564618f65
Sebapunisher <cossiofranco717@gmail.com>
2025-10-30 16:34:28 -0300

    cossio

--- tests/test_multiplicacion_Cossio.py
+++ tests/test_multiplicacion_Cossio.py
@@ -1,4 +1,4 @@
 #funciones/multiplicar.py 
 def multiplicarCossio(a, b): 
-  """Devuelve el producto de dos números.""" 
+   #"""Devuelve el producto de dos números.""" 
  return a * b # type: ignore
